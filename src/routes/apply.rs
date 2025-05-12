bc8ca194505f02326d97bfe65d47d840e0bb2d89
Brian Chen <enderchen004@outlook.com>
2025-05-12 09:10:47 +0100

    chore: Add copyright notices

--- src/routes/apply.rs
+++ src/routes/apply.rs
@@ -1,3 +1,18 @@
+// Enviame - Full-stack Priority Messenger with a Rust backend that respects priority settings and delivers messages.
+// Copyright (C) 2025 Brian Chen (differental)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 use askama::Template;
 use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
 use axum_csrf::CsrfToken;
